79a701a3d4a4951348723d5974f220ffa26abefb
Kate Sohng <kheeya7@hotmail.com>
2019-02-09 13:30:52 -0800

    Creating the SizeSelector component and props

--- app.js
+++ app.js
@@ -2,23 +2,36 @@
     "use strict";
 
     function ProductImage(props) {
-        // return React.createElement("img", {
-        //     src: "../assets/red.jpg",
-        //     alt: "Product red image"
-        // });
-        return <img src="../assets/red.jpg" alt="Product image" />
+        return <img src={`../assets/${props.color}.jpg`} alt="Product image" />
+    }
+
+    function SizeSelector(props) {
+        function sizeOptions() {
+            var sizes = window.Inventory.allSizes;
+            return sizes.map(function (num) {
+                return (
+                    <option value={num} key={num}>{num}</option>
+                );
+            });
+        }
+        return (
+            <div className="select-group">
+                <label htmlFor="size-options">Size: </label>
+                <select defaultValue={props.size} name="sizeOptions" id="size-options">
+                    {sizeOptions()}
+                </select>
+            </div>
+        )
     }
 
     function ProductCustomizer(props) {
-        // return React.createElement(
-        //     "div",
-        //     { className: "customizer" },
-        //     React.createElement("div", { className: "product-image" }, React.createElement(ProductImage))
-        // );
         return (
             <div className="customizer">
                 <div className="product-image">
-                    <ProductImage />
+                    <ProductImage color="blue" />
+                </div>
+                <div className="selector">
+                    <SizeSelector size={4} />
                 </div>
             </div>
         )
