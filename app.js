f08ae4c1edc72cbeba41148c52d1a168edbcd147
Kate Sohng <kheeya7@hotmail.com>
2019-02-09 08:28:46 -0800

    Added ProductCustomizer and ProductImage

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 (function () {
     "use strict";
 
-    ReactDOM.render(ProductCustomizer, document.getElementById("react-root"))
+    function ProductImage(props) {
+        return React.createElement("img", {
+            src: "../assets/red.jpg",
+            alt: "Product image"
+        });
+    }
+
+    function ProductCusmomizer(props) {
+        return React.createElement(
+            "div",
+            { className: "customizer" },
+            React.createElement("div", { className: "product-image" }, React.createElement(ProductImage))
+        );
+    }
+
+    ReactDOM.render(React.createElement(ProductCusmomizer), document.getElementById("react-root"))
 })();
